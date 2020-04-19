29488e790a4e6b9a57b742a898bc0ce38527b1dd
Katherine Cheng <kyc01@mit.edu>
2020-04-19 18:48:14 -0400

    add install to add paths

---
+++ install.m
@@ -0,0 +1,11 @@
+cd deriv
+addpath(genpath(pwd));
+cd ../MBO
+addpath (genpath(pwd));
+cd ../mesh
+addpath(genpath(pwd));
+cd ../visualize
+addpath(genpath(pwd));
+cd ../run
+addpath(genpath(pwd));
+savepath;
